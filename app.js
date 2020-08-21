3493f4628736562a9f4f1909c41091bd94590c02
Mudassir <mudassirsiddiqui727@gmail.com>
2020-08-21 23:53:58 +0500

    this is my first commit in second node app

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express')
+
+var app = express()
+
+var hbs = require('hbs')
+
+app.set('view engine', 'hbs')
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getTime', () => {
+    return new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds() + ' ' + new Date().toString()
+})
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        titlepage: 'Home'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        titlepage: 'About'
+    })
+})
+
+app.get('/blog', (req, res) => {
+    res.render('blog', {
+        titlepage: 'Blog'
+    })
+})
+
+app.listen(5000, () => {
+    console.log('you are listining at port 5000')
+})
